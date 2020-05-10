1f2b71a938fe7056188b87640bc0838963209e61
Rashidi Zin <rashidi@zin.my>
2020-05-10 10:07:16 +0800

    Attempt to increase test coverage

--- src/test/java/posmy/argos/desk/domain/DeskRepositoryTests.java
+++ src/test/java/posmy/argos/desk/domain/DeskRepositoryTests.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.groups.Tuple.tuple;
 import static posmy.argos.desk.domain.DeskArea.COLLABORATION;
 import static posmy.argos.desk.domain.DeskArea.DATA_AND_TECHNOLOGY;
 import static posmy.argos.desk.domain.DeskStatus.OCCUPIED;
@@ -23,14 +24,18 @@ class DeskRepositoryTests {
     @Test
     void findByStatus() {
         var desk = new Desk();
+        var location = new DeskLocation("D", 12);
 
         desk.setArea(DATA_AND_TECHNOLOGY);
-        desk.setLocation(new DeskLocation("D", 12));
+        desk.setLocation(location);
         desk.setStatus(VACANT);
 
         repository.save(desk);
 
-        assertThat(repository.findByStatus(VACANT)).hasSize(1);
+        assertThat(repository.findByStatus(VACANT))
+                .hasSize(1)
+                .extracting(Desk::getArea, Desk::getLocation, Desk::getStatus)
+                .containsOnly(tuple(DATA_AND_TECHNOLOGY, location, VACANT));
 
         assertThat(repository.findByStatus(OCCUPIED)).isEmpty();
     }
@@ -38,14 +43,18 @@ class DeskRepositoryTests {
     @Test
     void findByAreaAndStatus() {
         var desk = new Desk();
+        var location = new DeskLocation("D", 12);
 
         desk.setArea(DATA_AND_TECHNOLOGY);
-        desk.setLocation(new DeskLocation("D", 12));
+        desk.setLocation(location);
         desk.setStatus(VACANT);
 
         repository.save(desk);
 
-        assertThat(repository.findByAreaAndStatus(DATA_AND_TECHNOLOGY, VACANT)).hasSize(1);
+        assertThat(repository.findByAreaAndStatus(DATA_AND_TECHNOLOGY, VACANT))
+                .hasSize(1)
+                .extracting(Desk::getArea, Desk::getLocation, Desk::getStatus)
+                .containsOnly(tuple(DATA_AND_TECHNOLOGY, location, VACANT));;
 
         assertThat(repository.findByAreaAndStatus(COLLABORATION, VACANT)).isEmpty();
     }

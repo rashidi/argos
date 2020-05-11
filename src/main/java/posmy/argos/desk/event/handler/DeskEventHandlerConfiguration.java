65b3cc619518619870fa226fe18cdca54f107d81
Rashidi Zin <rashidi@zin.my>
2020-05-11 16:56:16 +0800

    Store information of occupied Desk

---
+++ src/main/java/posmy/argos/desk/event/handler/DeskEventHandlerConfiguration.java
@@ -0,0 +1,18 @@
+package posmy.argos.desk.event.handler;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import posmy.argos.desk.history.domain.DeskOccupiedHistoryRepository;
+
+/**
+ * @author Rashidi Zin
+ */
+@Configuration
+public class DeskEventHandlerConfiguration {
+
+    @Bean("deskAfterSaveEventHandler")
+    public DeskAfterSaveEventHandler afterSave(DeskOccupiedHistoryRepository historyRepository) {
+        return new DeskAfterSaveEventHandler(historyRepository);
+    }
+
+}

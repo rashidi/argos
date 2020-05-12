3ea843364dba5bd94c7c0ba4ae1cb805791c85ed
Rashidi Zin <rashidi@zin.my>
2020-05-12 09:55:55 +0800

    Add integration for occupy

--- src/main/java/posmy/argos/desk/domain/Desk.java
+++ src/main/java/posmy/argos/desk/domain/Desk.java
@@ -1,6 +1,10 @@
 package posmy.argos.desk.domain;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonSetter;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.experimental.Accessors;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -11,6 +15,8 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document
 @Data
 @Accessors(fluent = true)
+@Setter(onMethod = @__(@JsonSetter))
+@Getter(onMethod = @__(@JsonGetter))
 public class Desk {
 
     @Id
---
+++ src/test/java/posmy/argos/desk/domain/DeskRepositoryRestResourceTests.java
@@ -0,0 +1,83 @@
+package posmy.argos.desk.domain;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Example;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+import posmy.argos.desk.history.domain.DeskOccupiedHistory;
+import posmy.argos.desk.history.domain.DeskOccupiedHistoryRepository;
+import posmy.argos.security.azure.WithAzureADUser;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.hateoas.MediaTypes.HAL_JSON;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+import static posmy.argos.desk.domain.DeskArea.DATA_AND_TECHNOLOGY;
+import static posmy.argos.desk.domain.DeskStatus.OCCUPIED;
+import static posmy.argos.desk.domain.DeskStatus.VACANT;
+
+/**
+ * @author Rashidi Zin
+ */
+@SpringBootTest
+@WithAzureADUser
+class DeskRepositoryRestResourceTests {
+
+    @Autowired
+    private WebApplicationContext ctx;
+
+    private MockMvc mvc;
+
+    @Autowired
+    private DeskRepository repository;
+
+    @Autowired
+    private DeskOccupiedHistoryRepository historyRepository;
+
+    @BeforeEach
+    void setup() {
+        mvc = webAppContextSetup(ctx)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    void browse() throws Exception {
+        mvc.perform(
+                get("/desks")
+        )
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void occupy() throws Exception {
+        var location = new DeskLocation().row("D").column(12);
+
+        var desk = repository.save(
+                new Desk().location(location).area(DATA_AND_TECHNOLOGY).status(VACANT)
+        );
+
+        var content = new ObjectMapper().writeValueAsString(desk.status(OCCUPIED));
+
+        mvc.perform(
+                put("/desks/{id}", desk.id())
+                        .accept(HAL_JSON)
+                        .content(content)
+                        .contentType(APPLICATION_JSON)
+        )
+                .andExpect(status().isOk());
+
+        var history = historyRepository.findOne(Example.of(new DeskOccupiedHistory().location(location)));
+
+        assertThat(history)
+                .isNotEmpty();
+    }
+}

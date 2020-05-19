7505d1b06fcfc241d5c107e602733036bab3c18e
Rashidi Zin <rashidi@zin.my>
2020-05-19 09:13:11 +0800

    Clean up
    Closes gh-59.

--- src/main/java/posmy/argos/desk/domain/DeskLocation.java
+++ src/main/java/posmy/argos/desk/domain/DeskLocation.java
@@ -2,15 +2,15 @@ package posmy.argos.desk.domain;
 
 import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonSetter;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
 
+import java.util.Objects;
+
 /**
  * @author Rashidi Zin
  */
-@EqualsAndHashCode
 @Accessors(fluent = true)
 @Setter(onMethod = @__(@JsonSetter))
 @Getter(onMethod = @__(@JsonGetter))
@@ -19,5 +19,17 @@ public class DeskLocation {
     String row;
 
     Integer column;
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (!(obj instanceof DeskLocation)) {
+            return false;
+        }
+
+        var other = (DeskLocation) obj;
+
+        return Objects.equals(row, other.row) && Objects.equals(column, other.column);
+    }
 
 }

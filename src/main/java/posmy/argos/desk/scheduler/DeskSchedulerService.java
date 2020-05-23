8cc133592e79a4e42be206b244c3a0dcf0688d6c
Rashidi Zin <rashidi@zin.my>
2020-05-23 16:22:11 +0800

    Add scheduler to update inactive occupied desks
    See gh-52.

---
+++ src/main/java/posmy/argos/desk/scheduler/DeskSchedulerService.java
@@ -0,0 +1,53 @@
+package posmy.argos.desk.scheduler;
+
+import posmy.argos.desk.domain.Desk;
+import posmy.argos.desk.domain.DeskRepository;
+import posmy.argos.desk.history.domain.DeskOccupiedHistoryRepository;
+
+import java.util.ArrayList;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+import org.springframework.data.domain.Example;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.AllArgsConstructor;
+
+import static posmy.argos.desk.domain.DeskStatus.OCCUPIED;
+
+import static java.time.Instant.now;
+
+/**
+ * @author Rashidi Zin
+ */
+@Service
+@AllArgsConstructor
+public class DeskSchedulerService {
+
+    private final DeskRepository repository;
+
+    private final DeskOccupiedHistoryRepository historyRepository;
+
+    /**
+     * Find list of Desks that are marked as OCCUPIED but no longer being occupied.
+     */
+    @Transactional(readOnly = true)
+    protected Stream<Desk> findInactiveOccupiedDesks() {
+        var withStatusOccupied = Example.of(new Desk().status(OCCUPIED));
+        
+        var occupiedDesks = repository.findAll(withStatusOccupied);
+        var inactiveOccupiedDesks = new ArrayList<Desk>();
+
+        for (Desk desk : occupiedDesks) {
+            var isInactive = !historyRepository.existsByLocationAndEndAfter(desk.location(), now());
+
+            if (isInactive) {
+                inactiveOccupiedDesks.add(desk);
+            }
+        }
+
+        return inactiveOccupiedDesks.stream();
+    }
+
+}
--- src/test/java/posmy/argos/desk/history/domain/DeskOccupiedHistoryRepositoryTests.java
+++ src/test/java/posmy/argos/desk/history/domain/DeskOccupiedHistoryRepositoryTests.java
@@ -1,12 +1,15 @@
 package posmy.argos.desk.history.domain;
 
+import posmy.argos.desk.domain.DeskLocation;
+
+import java.util.List;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import posmy.argos.desk.domain.DeskLocation;
 
-import java.util.List;
+import static posmy.argos.desk.helper.DeskTestHelper.create;
 
 import static java.time.Instant.now;
 import static java.time.temporal.ChronoUnit.DAYS;
@@ -35,19 +38,28 @@ class DeskOccupiedHistoryRepositoryTests {
 
         repository.saveAll(List.of(
                 new DeskOccupiedHistory().occupant(occupant).location(location).since(yesterday).end(yesterdayEnd),
-                new DeskOccupiedHistory().occupant(occupant).location(location).since(today).end(todayEnd)
-        ));
+                new DeskOccupiedHistory().occupant(occupant).location(location).since(today).end(todayEnd)));
 
-        assertThat(repository.findFirstByLocationOrderByEndDesc(location))
-                .isNotEmpty()
-                .get()
-                .satisfies(persisted -> {
-                    assertThat(persisted.id()).isNotNull();
-                    assertThat(persisted.occupant()).isEqualTo(occupant);
-                    assertThat(persisted.location()).isEqualTo(location);
-                    assertThat(persisted.since()).isAfter(yesterday).isBeforeOrEqualTo(today);
-                    assertThat(persisted.end()).isAfter(yesterdayEnd).isBeforeOrEqualTo(todayEnd);
-                });
+        assertThat(repository.findFirstByLocationOrderByEndDesc(location)).isNotEmpty().get().satisfies(persisted -> {
+            assertThat(persisted.id()).isNotNull();
+            assertThat(persisted.occupant()).isEqualTo(occupant);
+            assertThat(persisted.location()).isEqualTo(location);
+            assertThat(persisted.since()).isAfter(yesterday).isBeforeOrEqualTo(today);
+            assertThat(persisted.end()).isAfter(yesterdayEnd).isBeforeOrEqualTo(todayEnd);
+        });
+    }
+
+    @Test
+    void existsByLocationAndEndAfter() {
+        var location = create().location();
+
+        repository.save(
+            new DeskOccupiedHistory().location(location).since(now().minus(9, HOURS)).end(now().minus(1, HOURS))
+        );
+
+        assertThat(repository.existsByLocationAndEndAfter(location, now().minus(2, HOURS))).isTrue();
+
+        assertThat(repository.existsByLocationAndEndAfter(location, now())).isFalse();
     }
 
     @AfterEach

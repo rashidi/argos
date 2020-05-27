70ac682c7a161e86d07b4fd906db7e17d310db07
Rashidi Zin <rashidi@zin.my>
2020-05-27 17:01:26 +0800

    Add Svelte

--- src/main/java/posmy/argos/security/OAuthSecurityConfiguration.java
+++ src/main/java/posmy/argos/security/OAuthSecurityConfiguration.java
@@ -1,13 +1,17 @@
 package posmy.argos.security;
 
 import com.microsoft.azure.spring.autoconfigure.aad.AADAppRoleStatelessAuthenticationFilter;
-import lombok.AllArgsConstructor;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import lombok.AllArgsConstructor;
+
 import static org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest.toAnyEndpoint;
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toStaticResources;
 import static org.springframework.security.config.http.SessionCreationPolicy.NEVER;
 
 /**
@@ -18,6 +22,12 @@ import static org.springframework.security.config.http.SessionCreationPolicy.NEV
 public class OAuthSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private final AADAppRoleStatelessAuthenticationFilter authenticationFilter;
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+            .ignoring().requestMatchers(toStaticResources().atCommonLocations());
+	}
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
